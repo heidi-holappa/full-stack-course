567d52d58a2b67b7f5bafb637bd09315f82b97c9
Heidi Holappa <heidi.holappa@helsinki.fi>
2022-07-24 19:21:49 +0300

    update task

--- part-2/countries/src/App.js
+++ part-2/countries/src/App.js
@@ -1,11 +1,45 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import Weather from './components/Weather'
 
-const Countries = ( { countries } ) => {
-  const result = countries
-    .map(country =>
-      <div key={Object.entries(country.latlng)}>{country.name.common}</div>)
-  return result
+const Countries = ( { countries, searchTerm, setNewSearch } ) => {
+  const filtered = countries
+  .filter(function(country){
+    return country.name.common.toLowerCase().includes(searchTerm.toLowerCase())
+  })
+
+  if (filtered.length === 1) {
+    const country = filtered[0]
+    return (
+      <div>
+        <h2>{country.name.common}</h2>
+        <p>
+          Capital: {country.capital}<br />
+          Area:  {country.area}
+        </p>
+        <strong>Languages</strong><br />
+        <ul>
+          {Object.entries(country.languages).map(language =>
+            <li key={language[0]}>{language[1]}</li>
+          )}
+        </ul>
+        <img src={country.flags.svg} width="200"></img>
+        <Weather capital={country.capital} />
+      </div>
+    )
+
+  } else if (filtered.length > 10) {
+    return (
+    <div>Too many matches, specify another filter.</div>
+    )
+  } else {
+    const handleClick = (event) => {
+      setNewSearch(event.target.value)
+    }
+    const result = filtered.map(country =>
+      <div key={Object.entries(country.cca3)}>{country.name.common} <button onClick={handleClick} value={country.name.common}>show</button></div>)
+    return result
+  }
 }
 
 const Filter = ({ searchTerm, handleSearchChange }) => {
@@ -24,20 +58,16 @@ function App() {
   const [countries, setCountries] = useState([])
   const [searchTerm, setNewSearch] = useState('')
 
-  const [restUrl, setRestUrl] = useState('https://restcountries.com/v3.1/all')
-
   useEffect(() => {
     axios
-      .get(restUrl)
+      .get('https://restcountries.com/v3.1/all')
       .then(response => {
         setCountries(response.data)
       })
-    })
+    }, [])
   
   const handleSearchChange = (event) => {
     setNewSearch(event.target.value)
-    console.log('https://restcountries.com/v3.1/name/' + event.target.value)
-    setRestUrl('https://restcountries.com/v3.1/name/' + event.target.value)
   }
   
   return(
@@ -46,12 +76,14 @@ function App() {
         searchTerm={searchTerm}
         handleSearchChange={handleSearchChange}
       />
-      <Countries countries={countries} />
+      <Countries 
+        countries={countries} 
+        searchTerm={searchTerm}
+        setNewSearch={setNewSearch}
+      />
     </div>
   )
     
 }
 
 export default App
-
-/* {countries.map(country => <div>{country.name.common}</div>)} */
---
+++ part-2/countries/src/components/Weather.js
@@ -0,0 +1,38 @@
+import axios from "axios"
+import { useState, useEffect } from "react"
+
+async function FetchWeather(setWeather, capital) {
+    const api_key = process.env.REACT_APP_API_KEY
+    useEffect(() => {
+        const weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${capital}&units=metric&appid=${api_key}`
+        axios
+            .get(weatherUrl)
+            .then(response => {
+                setWeather(response.data)
+            })
+    }, [])
+
+}
+
+const Weather = ({ capital }) => {
+    const [weather, setWeather] = useState([])
+    FetchWeather(setWeather, capital)
+
+    if (weather.length != 0) {
+        return (
+            <div>
+                <h2>Weather in {capital}</h2>
+                <p>temperature {weather.list[0].main.temp} Celcius</p>
+                <img src={`http://openweathermap.org/img/wn/${weather.list[0].weather[0].icon}@2x.png`} />
+                <p>wind {weather.list[0].wind.speed} m/s</p>
+            </div>
+        )
+    } else {
+        return (
+            <div>Fetching weather information, one moment.</div>
+        )
+    }
+    
+}
+
+export default Weather
