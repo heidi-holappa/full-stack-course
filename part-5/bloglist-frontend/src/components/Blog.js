cf1591279ac59e6c92bae6b02641b6567063033e
Heidi Holappa <heidi.holappa@helsinki.fi>
2022-08-21 19:49:48 +0300

    Add task 5.7. Stable version

--- part-5/bloglist-frontend/src/components/Blog.js
+++ part-5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,37 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
 
+const Blog = ({blog}) => {
+  const [expanded, setExpanded] = useState(false)
+  
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleExpanded = () => {
+    setExpanded(!expanded)
+  }
+
+  if (expanded) {
+    return (
+      <div style={blogStyle}>
+        {blog.title} {blog.author} <button onClick={toggleExpanded}>view</button><br></br>
+        {blog.url} <br></br>
+        likes 0 <button>like</button> <br></br>
+        {blog.user.name}
+      </div>
+    )
+  }
+
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author} <button onClick={toggleExpanded}>view</button>
+    </div>  
+  )
+
+}
 export default Blog
