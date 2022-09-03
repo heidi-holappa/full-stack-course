1febedd654bc45f0d3996456a7d921c1de619191
Heidi Holappa <heidi.holappa@helsinki.fi>
2022-09-03 10:43:34 +0300

    Update stable version of part 6 tasks

---
+++ part-6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -0,0 +1,11 @@
+import { useDispatch } from 'react-redux'
+import { newAnecdote } from '../reducers/noteReducer'
+
+
+const AnecdoteForm = (props) => {
+    const dispatch = useDispatch()
+
+
+}
+
+export default AnecdoteForm
